6320f00e08ddca3b49750e654804cd3c7a404357
Nishad Kindre <nishadkindre9553@gmail.com>
2022-07-14 18:17:06 +0530

    Update reverse.py

--- reverse.py
+++ reverse.py
@@ -1,18 +1,40 @@
-# reverse function to return the reversed version of indexed type of collections.
+# reverse functions to return the reversed version of indexed type of collections.
 
-def reverse_string(x: str) -> None:
+#? Using for loop :- 
+def reverse_string(x: str) -> str:
     '''Return a reversed copy of the entered string'''
+    new = ""
     for i in range(len(x)-1,-1,-1):
-        print(x[i], end="")
+        new += x[i]
+    return new
 
-def reverse_list(x: list) -> None:
+def reverse_list(x: list) -> list:
+    '''Return a reversed copy of the entered list'''
     new = []
     for i in range(len(x)-1,-1,-1):
         new.append(x[i])
-    print(new)
+    return new
 
-def reverse_tuple(x: tuple) -> None:
+def reverse_tuple(x: tuple) -> tuple:
+    '''Return a reversed copy of the entered tuple'''
     new = []
     for i in range(len(x)-1,-1,-1):
         new.append(x[i])
-    print(tuple(new))
+    return tuple(new)
+
+#? Using while loop :- 
+def reverse_string_(x: str) -> str:
+    new = ""
+    i = -1
+    while i>= -len(x):
+        new += x[i]
+        i -= 1
+    return new
+
+def reverse_list_(x: list) -> list:
+    new = []
+    i = -1
+    while i>= -len(x):
+        new.append(x[i])
+        i -= 1
+    return new
